68e567ea47d5676eb6137a0ab95ead04686bb6ed
Ritwick Verma <verma1997@gmail.com>
2018-10-05 11:39:29 +0530

    03

--- boot_sect_simple.asm
+++ boot_sect_simple.asm
@@ -1,16 +1,23 @@
+;[org 0x7c00]
+
+the_secret:
+    db "X"
+
 mov ah,0x0e
-mov al, 'H'
+mov al, the_secret
 int 0x10
-mov al, 'e'
+mov al, [the_secret]
 int 0x10
-mov al,'l'
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
-int 0x10
-mov al,'o'
+mov bx, 2d
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
 
 jmp $
 
 times 510-($-$$) db 0
 dw 0xaa55
-
